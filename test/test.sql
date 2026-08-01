33c7a10ef5dd65cc528a3b267011ff65f88377d7
Gaireddy Srikanth <gaireddysrikanth5@gmail.com>
2026-08-01 11:38:29 +0530

    Update test.sql

--- test/test.sql
+++ test/test.sql
@@ -1,23 +1,57 @@
--- Querys test cases silver.crm_cust_info
-
--- let checks the unwanted spaces for Firstname, lastname and gender
-
-select cst_firstname from silver.crm_cust_info
-where cst_firstname != trim(cst_firstname);
-
-select cst_lastname from silver.crm_cust_info
-where cst_lastname != trim(cst_lastname);
-
-select cst_gndr from silver.crm_cust_info
-where cst_gndr != trim(cst_gndr);
+-- Queries to test silver.crm_cust_info 
 
 
--- let check the values in gender columns
-select distinct cst_gndr from silver.crm_cust_info;
 
 
--- marital_status replace names married or single
-select distinct cst_marital_status from silver.crm_cust_info;
+
+
+
+
+
+-- Writing a query to check if any duplicates are there in pk(cst_id) or not
+SELECT
+cst_id,
+count(*) 
+from silver.crm_cust_info
+group by cst_id
+having count(*) > 1 or cst_id is null;
+
+
+
+--check for unwanted spaces in firstname
+
+SELECT cst_firstname from silver.crm_cust_info
+WHERE cst_firstname != trim(cst_firstname)
+
+
+--check for unwanted spaces in lastname
+
+SELECT cst_lastname from silver.crm_cust_info
+WHERE cst_lastname != trim(cst_lastname)
+
+-- Lets check the values in gender columns
+
+SELECT DISTINCT cst_gndr
+from silver.crm_cust_info;
+
+
+-- Lets check the values in marital status col 
+
+SELECT DISTINCT cst_marital_status
+from silver.crm_cust_info;
+
+
+
+
+
+-- Test for silver.crm.prd_info 
+
+
+
+
+
+
+
 
 
 
@@ -29,53 +63,81 @@ group by prd_id
 having count(*) > 1 or prd_id is null;
 
 
-SELECT *,
-REPLACE( SUBSTRING(prd_key,1,5),'-','_') as cat_id,
-SUBSTRING(prd_key,7,len(prd_key)) as cat_id
-FROM bronze.crm_prd_info;
-
-
 -- Check if prd_nm has any unwanted spaces
+
 SELECT prd_nm
 from bronze.crm_prd_info
-where prd_nm != Trim(prd_nm);
+where prd_nm != Trim(prd_nm)
 
+-- Check of prd_cost has any null 
 
---
-select *, 
-REPLACE( SUBSTRING(prd_key,1,5),'-','_') as cat_id, 
-SUBSTRING(prd_key,7,len(prd_key)) as cat_id , 
-ISNULL(prd_cost,0 ) as prd_cost_values, 
-CASE when UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain' 
-when UPPER(TRIM(prd_line)) = 'R' THEN 'Road' 
-when UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales' 
-when UPPER(TRIM(prd_line)) = 'T' THEN 'Touring' 
-else 'n/a' end as prd_line from bronze.crm_prd_info;
-
+SELECT prd_cost
+from bronze.crm_prd_info
+where prd_cost IS NULL;
 
 
 
 -- Lets check if the start date is less than end date
+
+
 SELECT * from bronze.crm_prd_info
-where prd_end_dt < prd_start_dt ;
+where prd_start_dt < prd_end_dt;
 
 
--- Replace PRD_COST with 0
 
-SELECT prd_id,
-prd_nm,
-REPLACE( SUBSTRING(prd_key,1,5),'-','_') as cat_id,
-SUBSTRING(prd_key,7,len(prd_key)) as prd_id ,
-ISNULL(prd_cost,0 ) as prd_cost_values,
-CASE
- when UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
- when UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
- when UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
- when UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
- else 'n/a'
-END AS prd_line,
-cast(prd_start_dt as date) as prd_start_dt,
-CAST(lead(prd_start_dt) OVER(PARTITION BY prd_key order by prd_start_dt) - 1 as DATE)
-AS PRD_END_DATE
-FROM bronze.crm_prd_info;
 
+
+
+
+-- test for crm_sales_details
+
+
+
+
+-- Checking for invalid dates for sls_order_dt
+SELECT
+sls_order_dt
+from bronze.crm_sales_details
+where sls_order_dt <= 0 or len(sls_order_dt) != 8 or sls_order_dt is null
+
+-- Checking for invalid dates for sls_ship_dt
+SELECT
+sls_ship_dt
+from bronze.crm_sales_details
+where sls_ship_dt <= 0 or len(sls_ship_dt) != 8 or sls_ship_dt is null
+-- Lets check for due_dt
+SELECT
+sls_due_dt
+from bronze.crm_sales_details
+where sls_due_dt <= 0 or len(sls_due_dt) != 8 or sls_due_dt is null;
+
+SELECT * from bronze.crm_sales_details
+where sls_sales is null or sls_sales !=  sls_quantity * sls_price  ;
+
+
+SELECT * from bronze.crm_sales_details
+where sls_price is null or sls_price !=  sls_sales / sls_quantity  ;
+
+SELECT * from bronze.crm_sales_details
+where sls_quantity is null or sls_quantity !=  sls_sales / sls_price  ;
+
+
+
+
+
+
+
+-- tests for erp_cust_az12
+
+
+
+-- Lets get the diff types of values in gender
+select distinct gen
+from bronze.erp_cust_az12;
+
+
+-- getting invalud bdates
+SELECT DISTINCT
+bdate
+from bronze.erp_cust_az12
+where bdate < '1926-01-01' or bdate > getdate()
